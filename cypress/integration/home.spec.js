6d3a864b92f391caedf4b5727fd704f7bca8dadf
arthur <arthurkenned21@gmail.com>
2022-06-08 22:39:51 -0300

    tirando o visit

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,6 @@
 describe('Home page',()=>{
     it('app shold estar online' , ()=>{
-      cy.visit('https://buger-eats.vercel.app')
+      cy.visit('/')
       cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     }) 
 })
